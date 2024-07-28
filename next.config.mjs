4f7e6f04af8a96baa91aa4c4fd4edd9f8e8db166
LucasJnoub <lucas.jnoub@gmail.com>
2024-07-28 01:55:38 -0300

    m

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPwa from 'next-pwa';
 
-export default nextConfig;
+const withPWA = nextPwa({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+export default withPWA({
+  // outras configurações do Next.js
+});
